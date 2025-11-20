584aa87d4ef6184da07d20c9a0bd0db0810c0f4e
bhavyaraj01 <bhavyarajsinghjhala40@gmail.com>
2025-11-20 07:32:39 +0530

    just try

--- ad classes/question_1.cpp
+++ ad classes/question_1.cpp
@@ -1,7 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
-            
-int main(){
-                    
-return 0;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> dec(n), jan(n), diff(n);
+    
+    vector<string> names(n);
+
+    for(int i=0;i<n;i++){
+        cin>>names[i]>>dec[i]>>jan[i];
+         diff[i]=jan[i]-dec[i];
+    }
+    
+    //check
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(diff[i]>)
+        }
+    }
+    
+
+        return 0;
 }
+
